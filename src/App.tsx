4d64cfe34b9e664982ff030b829b6e44990722b2
Irene Papaspyratos <irene.papaspyratos@gmail.com>
2022-07-22 06:33:03 +0200

    Update app header

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import './global.css';
+import headerBackground from './headerBackground.png'
+import Gallery from "./components/Gallery/Gallery";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-      <main>
-      </main>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={headerBackground}
+                     alt="rickAndMortyPicture"
+                     height="248px"
+                     width="640px"/>
+            </header>
+            <main>
+            </main>
+        </div>
+    );
 }
 
 export default App;
